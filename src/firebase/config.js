42bee166c6a045a3d91eb959fb553b7c6b62a0b8
SugoiServal <BorisLihere@gmail.com>
2022-08-12 17:20:31 -0400

    imported hooks and context tools from another time

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAk6Uhi6OZ57Nd7z-pK1ubHglmOh69uXWQ",
+  authDomain: "project-management-b274f.firebaseapp.com",
+  projectId: "project-management-b274f",
+  storageBucket: "project-management-b274f.appspot.com",
+  messagingSenderId: "783942725487",
+  appId: "1:783942725487:web:4d1e27d342931e786b429a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
